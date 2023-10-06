a914e10416c58c7199e596710c99121d5486d199
Viktor <viktorgjurcheski@gmail.com>
2023-10-06 16:37:38 +0200

    commit all files

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+// import macro - exports macro for handling action for exporting functions to javascript
+use wasm_bindgen::prelude::*;
+use web_sys::console::log_1 as log;
+use base64::{encode, decode};
+use image::load_from_memory;
+use image::ImageOutputFormat::Png;
+
+
+#[wasm_bindgen]
+pub fn grayscale(encoded_file: &str) -> String {
+    // .into helps detect data type to convert to (string to js)
+    log(&"Grayscale called".into());
+
+    let base64_to_vector = decode(encoded_file).unwrap();
+    log(&"Image decoded".into());
+
+    let mut img = load_from_memory(&base64_to_vector).unwrap();
+    log(&"Image loaded".into());
+
+    img = img.grayscale();
+    log(&"Grayscale effect applied".into());
+
+    let mut buffer = vec![];
+    img.write_to(&mut buffer, Png).unwrap();
+    log(&"New image written".into());
+
+    let encoded_img = encode(&buffer);
+    let data_url = format!(
+        "data:image/png;base64,{}",
+        encoded_img
+    );
+
+    data_url
+}
